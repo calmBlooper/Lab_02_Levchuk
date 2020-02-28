945349b4191bdfcbc6c7f76204849f39ad093cd5
calmBlooper <pifagor6541@gmail.com>
2020-02-28 23:57:05 +0200

    Final touch before commiting TSR war crimes in Kharkiv

---
+++ Lab_02_Levchuk/Models/Person.cs
@@ -0,0 +1,143 @@
+﻿using System;
+
+namespace Lab_02_Levchuk.Models
+{
+    class Person
+    {
+        private String _name, _surname, _email;
+        private DateTime _birthDay;
+        //Eastern zodiacs helping array, for easier and faster computation of the "ChineseSign" method
+        private  string[] _chineseZodiacs = { "Monkey", "Rooster", "Dog", "Pig", "Rat", "Ox", "Tiger", "Rabbit", "Dragon", "Snake", "Horse", "Goat" };
+        public Person(string name, string surname, string email)
+        {
+            _name = name;
+            _surname = surname;
+            _email = email;
+        }
+
+        public Person(string name, string surname, string email, DateTime birthDay)
+        {
+            _name = name;
+            _surname = surname;
+            _email = email;
+            _birthDay = birthDay;
+        }
+        public Person(string name, string surname, DateTime birthDay)
+        {
+            _name = name;
+            _surname = surname;
+            _birthDay = birthDay;
+        }
+        public String Name
+        {
+            set => _name = value;
+            get => _name;
+        }
+        public String Surname
+        {
+            set => _surname = value;
+            get => _surname;
+        }
+        public String Email
+        {
+            set => _email = value;
+            get => _email;
+        }
+        public DateTime BirthDay
+        {
+            set => _birthDay = value;
+            get => _birthDay;
+        }
+        public bool IsAdult {
+            get
+            {
+                int Age = DateTime.Now.Year - _birthDay.Year;
+                if (_birthDay.AddYears(Age) > DateTime.Now) Age--;
+                return Age > 18;
+            }
+        }
+        public string SunSign
+        {
+            get
+            {
+                int Month = _birthDay.Month, Day = _birthDay.Day;
+                switch (Month)
+                {
+                    case 1:
+                        if (Day < 20)
+                            return "Capricorn";
+                        else
+                            return "Aquarius";
+                    case 2:
+                        if (Day < 19)
+                            return "Aquarius";
+                        else
+                            return "Pisces";
+                    case 3:
+                        if (Day < 21)
+                            return "Pisces";
+                        else
+                            return "Aries";
+                    case 4:
+                        if (Day < 20)
+                            return "Aries";
+                        else
+                            return "Taurus";
+                    case 5:
+                        if (Day < 21)
+                            return "Taurus";
+                        else
+                            return "Gemini";
+                    case 6:
+                        if (Day < 21)
+                            return "Gemini";
+                        else
+                            return "Cancer";
+                    case 7:
+                        if (Day < 23)
+                            return "Cancer";
+                        else
+                            return "Leo";
+                    case 8:
+                        if (Day < 23)
+                            return "Leo";
+                        else
+                            return "Virgo";
+                    case 9:
+                        if (Day < 23)
+                            return "Virgo";
+                        else
+                            return "Libra";
+                    case 10:
+                        if (Day < 23)
+                            return "Libra";
+                        else
+                            return "Scorpio";
+                    case 11:
+                        if (Day < 22)
+                            return "Scorpio";
+                        else
+                            return "Sagittarius";
+                    default:
+                        if (Day < 22)
+                            return "Sagittarius";
+                        else
+                            return "Capricorn";
+                }
+            }
+        }
+public string ChineseSign {
+            get
+            {
+                return _chineseZodiacs[_birthDay.Year % 12];
+            }
+            }
+public bool IsBirthday
+        {
+            get
+            {
+                return DateTime.Now.Month == _birthDay.Month && DateTime.Now.Day == _birthDay.Day;
+            }
+        }
+    }
+}
---
+++ Lab_02_Levchuk/ViewModels/MainVM.cs
@@ -0,0 +1,131 @@
+﻿using Lab_02_Levchuk.Models;
+using Lab_02_Levchuk.Tools;
+using System;
+using System.ComponentModel;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Lab_02_Levchuk.ViewModels
+{
+    class MainVM: INotifyPropertyChanged
+    {
+      
+        private Person _userObject;
+        private string _name = "", _surname = "", _email = "";
+        private DateTime? _chosenDate;
+        public MainVM()
+        {
+            ButtonCommand = new RelayCommand(o => MainButtonClick("MainButton"));
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        public string Name
+        {
+            set
+            {
+                _name = value;
+                OnPropertyChanged("ButtonEnabled");
+            }
+                get => _name;
+        }
+        public string Surname
+        {
+            set
+            {
+                _surname = value;
+                OnPropertyChanged("ButtonEnabled");
+            }
+                get => _surname;
+        }
+        public string Email
+        {
+            set
+            {
+                _email = value;
+               OnPropertyChanged("ButtonEnabled");
+            }
+            get => _email;
+        }
+        public DateTime? ChosenDate {
+        set
+            {
+                _chosenDate = value;
+                OnPropertyChanged("ButtonEnabled");
+            }
+            get => _chosenDate;
+        }
+        public bool ButtonEnabled
+        {
+            get => !string.IsNullOrWhiteSpace(Name) && !string.IsNullOrWhiteSpace(Surname) && !string.IsNullOrWhiteSpace(Email)&&ChosenDate.HasValue; 
+        }
+        public string UserInfo { set; get; } = "";
+        public Visibility LoaderVisibility { set; get; } = Visibility.Collapsed;
+        public ICommand ButtonCommand { get; set; }
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        }
+        private int GetAge(DateTime enteredDate)
+        {
+            int Age = DateTime.Now.Year - enteredDate.Year;
+            if (enteredDate.AddYears(Age) > DateTime.Now) Age--;
+            return Age;
+        }
+        private bool BirthdayCorrect(DateTime enteredDate)
+        {
+            if (enteredDate.Year < DateTime.Now.Year)   return GetAge(enteredDate)<=135;
+            if (enteredDate.Year == DateTime.Now.Year&& enteredDate.Month < DateTime.Now.Month) return GetAge(enteredDate) < 135;
+            if (enteredDate.Year == DateTime.Now.Year && enteredDate.Month == DateTime.Now.Month && enteredDate.Day < DateTime.Now.Day) return GetAge(enteredDate) < 135;
+            return false;
+        }
+    
+        
+        private async void MainButtonClick(object sender)
+        {
+            ShowLoader();
+            await Task.Run(() =>
+            {
+                if (BirthdayCorrect(ChosenDate.Value))
+                {
+                   if (_userObject==null) _userObject = new Person(Name, Surname, Email, ChosenDate.Value);
+                   else
+                    {
+                        _userObject.Name = Name;
+                        _userObject.Surname = Surname;
+                        _userObject.Email = Email;
+                        _userObject.BirthDay = ChosenDate.Value;
+                    }
+                    UserInfo = "Name: " + _userObject.Name +
+                    "\nSurname: " + _userObject.Surname +
+                    "\nEmail: " + _userObject.Email +
+                    "\nDate of birth: " + _userObject.BirthDay.ToShortDateString() +
+                    "\nIs adult: " + (_userObject.IsAdult ? "Yes" : "No") +
+                    "\nSun sign: " + _userObject.SunSign +
+                    "\nChinese sign: " + _userObject.ChineseSign +
+                    "\nIs birthday today: " + (_userObject.IsBirthday ? "Yes" : "No");
+                    OnPropertyChanged("UserInfo");
+                }
+                else
+                {
+                    UserInfo = "";
+                    OnPropertyChanged("UserInfo");
+                }
+            });
+            HideLoader();
+            if (!BirthdayCorrect(ChosenDate.Value))  MessageBox.Show("Your entered date of birth is wrong.\nYou can not be less than 1 day old, or older than 135 years old.");
+            else if (_userObject.IsBirthday) MessageBox.Show("Hey, it`s your birthday today! Congratulations!");
+        }
+        private void ShowLoader()
+        {
+            LoaderVisibility = Visibility.Visible;
+            OnPropertyChanged("LoaderVisibility");
+        }
+        private void HideLoader()
+        {
+            LoaderVisibility = Visibility.Collapsed;
+            OnPropertyChanged("LoaderVisibility");
+        }
+    }
+}
